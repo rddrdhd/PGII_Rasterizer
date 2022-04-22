afbbe682f679d52d667119f32aa23214c1bd2eec
rddrdhd <53045239+rddrdhd@users.noreply.github.com>
2022-04-22 17:02:59 +0200

    visual studio stuff - idk what to do with it

---
+++ src/pg2_opengl/pg2_opengl/pg2_opengl.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "tutorials.h"
+#include "my_main.h"
+
+int main()
+{
+	printf( "PG2 OpenGL, (c)2020-2022 Tomas Fabian\n\n" );
+
+	//return tutorial_1();
+	//return tutorial_2();
+	//return tutorial_3( "../../../data/environments/abandoned_hopper_terminal_04_1k.jpg" );
+	//return tutorial_4( "../../../data/environments/abandoned_hopper_terminal_04_1k.exr" );
+	//return tutorial_5();
+	//return tutorial_6();
+	//return tutorial_7( "../../../data/adjacent_triangles.obj" );
+	return run();
+}
